79fe6d113716d5a898a0886e4e6b47bbacab3d3b
Pedro Paulo <ppcamargo82@gmail.com>
2024-08-16 02:48:36 -0300

    exercicio_um_terminado

--- MinhaClasseTeste/src/App.java
+++ MinhaClasseTeste/src/App.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class App {
     public static void main(String[] args) throws Exception {
        int idade = 25;
@@ -6,5 +8,6 @@ public class App {
        System.out.println("Ola, seu nome é: " + nome);
        System.out.println("Seu sexo é: " + sexo);
        System.out.println("E por fim sua idade é: " + idade);
+       Locale.setDefault(Locale.US);
     }
 }
